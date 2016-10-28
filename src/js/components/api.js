41ea309a5c4e38c71a16d201965ec162268d9d63
Montague Totten <monty.n.totten@gmail.com>
2016-10-28 15:45:12 -0400

    moved getWeatherInfo function from main.js to own js file and linked to index.html

--- src/js/components/api.js
+++ src/js/components/api.js
@@ -1,10 +1,12 @@
 var api = {
 	url: 'http://api.openweathermap.org/data/2.5/find?lat=34.0&lon=-81.0&cnt=10&appid=94d368f1ffc1c294a2ce41c5d6734883',
-	getWeatherInfo: function (cb) {
+	getWeatherInfo: function () {
 		$.ajax({
 			url: api.url,
 			success: function (results) {
-				cb(results);
+				app(results.list);
+				$('.container').fadeOut('slow');
+				console.log(results);
 			}
 		});
 	}
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,6 @@
-var url = 'http://api.openweathermap.org/data/2.5/find?lat=34.0&lon=-81.0&cnt=10&appid=94d368f1ffc1c294a2ce41c5d6734883';
 
 
-var getWeatherInfo = function () {
-	$.ajax({
-		url: url,
-		success: function (results) {
-			app(results.list);
-			$('.container').fadeOut('slow');
-			console.log(results);
-		}
-	});
-};
-
-getWeatherInfo();
+api.getWeatherInfo();
 
 function app(data) {
 	var appView = new AppView('div', data);
